046e7a2ce6131c5fe4c74c5d1b8541cd3fb6c06a
Bruno Graciano Alves Carneiro <bruno.graciano@targetsoftware.com.br>
2018-08-07 16:30:34 -0300

    Commit
    login realizado com sucesso

--- src/test/java/qa/cnajobs/pages/LoginPage.java
+++ src/test/java/qa/cnajobs/pages/LoginPage.java
@@ -1,5 +1,36 @@
 package qa.cnajobs.pages;
 
+import org.openqa.selenium.By;
+
+import qa.cnajobs.core.DSL;
+
 public class LoginPage {
+	
+	private DSL dsl = new DSL();
+	
+	public void SetEscreveEmail(String email) {
+		dsl.escreveId("Username", email);
+	}
+	
+	public void SetEscreverSenha(String senha) {
+		dsl.escreveId("Password", senha);
+	}
+	
+	public void SetClicarEntrar() {
+		dsl.clicarBotaoBy(By.cssSelector("button"));
+	}
+	
+	public void url(String url) {
+		dsl.maximizaJanela();
+		dsl.url(url);
+	}
+	
+	public void SetValidarAcesso(String texto) {
+		dsl.validarAcesso(texto, By.cssSelector(".click-animate .cna-local"));
+	}
+	
+	public void AguardarCarregarPagina(int tempo) throws InterruptedException {
+		dsl.esperaCarregar(tempo);
+	}
 
 }
--- src/test/java/qa/cnajobs/tests/LoginTest.java
+++ src/test/java/qa/cnajobs/tests/LoginTest.java
@@ -1,5 +1,22 @@
 package qa.cnajobs.tests;
 
+import org.junit.Test;
+
+import qa.cnajobs.pages.LoginPage;
+
 public class LoginTest {
+	
+	private LoginPage page = new LoginPage();
+	
+	@Test
+	public void DeveRealizarAcessoUsuario() throws InterruptedException {
+		
+		page.url("http://server.target1.com.br:8882");
+		page.SetEscreveEmail("danilozanutto@gmail.com");
+		page.SetEscreverSenha("123456");
+		page.SetClicarEntrar();
+		page.AguardarCarregarPagina(3000);
+		page.SetValidarAcesso("ADMINISTRAÇÃO NACIONAL CNA");
+	}
 
 }
